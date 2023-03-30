9bc90f0f5949eb79760840cfa20d79f9ea134bdd
JoseCortesG <josecortesalc@gmail.com>
2023-03-30 12:07:05 +0200

    PAso 5 completo

--- my-app/src/app/hero-detail/hero-detail.component.ts
+++ my-app/src/app/hero-detail/hero-detail.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -12,24 +10,19 @@ import { HeroService } from '../hero.service';
 })
 export class HeroDetailComponent implements OnInit {
   hero: Hero | undefined;
-
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
   ) {}
-
   ngOnInit(): void {
     this.getHero();
   }
-
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
-
-
   goBack(): void {
     this.location.back();
   }
